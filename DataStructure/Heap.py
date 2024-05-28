d1c746aad289b608be785f1caec3ee743db5207f
Yoon <151509222+yoonB-dev@users.noreply.github.com>
2024-05-28 22:33:53 +0900

    24.05.28

--- DataStructure/Heap.py
+++ DataStructure/Heap.py
@@ -1,1 +1,26 @@
 #Heap
+from queue import PriorityQueue
+
+class Heap:
+    def __init__(self):
+        self.heap = []
+        self.heap.append(None)
+
+
+Q1 = PriorityQueue()
+
+Q1.put(10)
+Q1.put(1)
+Q1.put(5)
+Q1.put(3)
+
+Q2 = PriorityQueue()
+Q2.put((5,'Lee'))
+Q2.put((10,'Yoon'))
+
+print(Q1.get())
+print(Q1.get())
+print(Q1.get())
+print(Q2.get())
+print(Q2.get()[1])
+
